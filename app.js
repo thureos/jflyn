ed3663fc8d90233362beed91ce1d74bd8bffec9a
Luis Barron <luis84barron@gmail.com>
2024-12-04 18:48:28 -0600

    variable for traits

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ function roulette() {
     const items = document.querySelectorAll('#roulette li');
     const itemHeight = items[0].offsetHeight;
     const totalItems = items.length;
-    const randomIndex = Math.floor(Math.random() * totalItems);
+    // const randomIndex = Math.floor(Math.random() * totalItems);
+    const randomIndex = Math.floor((109/113) * totalItems);
     
     // Calculate the final position
     const finalPosition = -(randomIndex * itemHeight);
 
     // Set the transform property to move the roulette
     roulette.style.transform = `translateY(${finalPosition}px)`;
-  }
+}
+
+function fillTraits() {
+  let traits = ['jewish','Republic','Cotton','Progressive','Handcrafted','Serbia','1080p','Cambridgeshire','customized','Cheese','Devolved','Assistant','withdrawal','alarm','Arizona','Bacon','Architect','Generic','SAS','enhance','ability','Zimbabwe','index','Benin','digital','Towels','Gloves','Networked','analyzer','rich','holistic','Loan','invoice','markets','withdrawal','algorithm','analyzing','transitional','Dynamic','Mobility','Connecticut','background','Africa','Zimbabwe','24/7','optical','Wooden','Concrete','Views','Generic','sensor','synergies','Peso','Turnpike','Principal','New','multi-state','neural','back-end','Dynamic','programming','multi-byte','conglomeration','standardization','Global','Pass','District','Berkshire','calculate','Analyst','Metal','Shirt','overriding','Orchestrator','SAS','Generic','Ouguiya','Avon','process','Cambridgeshire','Center','Handcrafted','Representative','implementation','cross-platform','Sports','JBOD','Shoes','payment','input','methodology','array','strategize','Ergonomic','Delaware','withdrawal','Investment','Concrete','invoice','Cotton','turquoise','Brunei','Maryland','Home','Shore','programming','harness','generate','mexican','fault-tolerant','Light','Paradigm'];
+
+  const ulRoulette = document.getElementById('roulette');
+  
+  traits.forEach(function (v) {
+    const myLi = document.createElement('li');
+    myLi.textContent = v;
+  
+    ulRoulette.appendChild(myLi);
+  });
+}
 
 (function() {
 
@@ -20,7 +34,7 @@ function roulette() {
     el.classList.remove('transparent');
   });
 
-
+  fillTraits()
   roulette()
 
 })()
